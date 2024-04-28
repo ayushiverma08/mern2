f7ad9f74d80d0abbdd9c19d404807c1c726d83fe
ayushiverma08 <95586944+ayushiverma08@users.noreply.github.com>
2024-04-28 22:51:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,136 @@
-//this is the one way to get result by changing it 
-//into string
-// const fs = require('node:fs');
+// const express = require('express');
+// //const fs = require("fs");
+// const fsPromises = require("fs/promises");
+// const app = express();
+// app.use(express.json());
+// app.get('/api/products' , (req ,res)=>{
+//   res.send("....HOMEpage....")
+//   console.log(req);
+//   res.send('work in progress');
 
-// const data = fs.readFileSync('./myread.txt');
-// console.log(data);
+      
+//     const arr=await fsPromises.readFile('./data.json',"utf8");
+//     const obj =JSON.parse(data).products;
+//     const len =arr.length;
+//         if(len==0){
+//           data.id=1;
+//           console.log(arr);
+//           arr.push(data);
+//           console.log(arr) 
+//         }
+//         else{
+//           const newId=db[len-1];
+//         }
+//       res.json(
+//         {
+//       status:'success',
+//       results: arr.length,
+//       data:{
+//         products:arr
+//       }
 
-// console.log(data.toString());
+//       app.post('/api/products' , (req ,res)=>{
+//         //console.log(Object.keys(req));
+//         //console.log(req.body);
+//         const data =req.body;
+//         data.id=121;
+//         console.log(data);
+//         const db = fsPromises.readFile(",/data.json",)
+        
+//   res.send('work in progress');
 
-
-// //second method to tell that it is string by encoding way
-// //using command utf8
-// // const fs = require('node:fs');
-// const fs = require('node:fs'); //like this also
-// const data = fs.readFileSync('./myread.txt',{encoding:'utf8'});
-// console.log(data);
-
-
-
-// BUFFERRRRRRRRRRRRRRRRRRRR
-// const b= new Buffer.from('ABCD');
-// b.write('other');
-// console.log(b.toString());
-
-
-
-// const fs = require('fs');
-// FileSystem.writeFileSync('./myread.txt',"10th april 2024:day7");
-// console.log(data);
-
-
-// const fs = require('fs');
-// console.log("start");
-// const data = fs.readFileSync('./myread.txt',{encoding:'utf8'});
-// console.log(data);
-// console.log("end");
-
-
-//promises in file system
-// const fsPromises = require('fs/promises');
-// console.log("start")
-// const pr = fsPromises.readFile("./myread.txt",{encoding:'utf8'});
-// console.log(pr)
-// pr.then((res)=>{
-//     console.log(res);
-// })
-// console.log("end");
+// // app.listen(1400,()=>{
+// //   console.log("....server started.....");+
+//       });
 
 
+ const express=require('express');
+// const fs=require('fs');
+const app=express();
+//middleware
+app.use(express.json());
+const fsPromises=require("fs/promises");
+app.get('/api/products',async(req,res)=>
+{
+    // const data=fs.readFileSync('./data.json',"utf8")
+    
+     const data= await fsPromises.readFile('./data.json',"utf8")
+    const arr=JSON.parse(data).products;
+    res.json({
+        status:'succes',
+        results:arr.length,
 
-//callback in file system
-// const fs = require('fs');
-
-// fs.readFile('./myread.txt',{encoding:'utf8'},(err,data)=>{
-//     console.log(data);
-// });
-
-
-
-//http://localhost:1400/
-
-const http = require('http');
-const fs=require('fs');
-const data=fs.
-const htmlTemplate=`
-<!DOCTYPE html>
-<html>
-<head>
-<style></style>
-
-</head>
-<body>
-    ....PRODUCT CARDS....
-</body>
-</html>
-
-`
-const cardTemplate=`
-<div class='product-card'>
-<h4>_TITLE_</h4>
-<p>_INFO_</p>
-</div>
-`
-const card1 = cardTemplate.replace('_TITLE_','XIOMI Note 11 pro')
-.replace('_INFO_','this is the chinese mobile');
-
-const card2 = cardTemplate.replace('_TITLE_','iphone Note 11 pro')
-.replace('_INFO_','this is the  mobile');
-
-const allcard=card1+card2
-console.log("\n:card1",card1)
-console.log("\n:card2",card2)
+        data:{
+            products:arr,
+        }
+    });
 
 
-const page = htmlTemplate.replace('_PRODUCT_CARDS_',card1);
-const app = http.createServer((req,res)=>{
-// console.log("request received");
-console.log(req.url);
-res.writeHead(200,{'content-type':'text/html',})
-res.end (page);
-// res.end;   //('<h2>hello!</h2>')
- });
-app.listen(2200,()=>{
-console.log("..........server started......."); 
-//greater than 1000 lesser than 9999
+});
+    app.post('/api/products',async(req,res)=>
+    {
+       const data=req.body;
+    //    data.id=121;
+    //     res.send(data);
+
+        const db= await fsPromises.readFile("./data.json","utf8");
+        const arr=JSON.parse(db);
+        const len=arr.length;
+        const newProduct=data;
+        if(len==0)
+        {
+          newProduct.id=1;
+            // data.id=1
+            // arr.push(newProduct);
+            // console.log(arr);
+           
+        }
+else{
+  // const newProduct=data;
+  newProduct.id=(arr[len-1].id)+1;
+  // arr.push(newProduct);
+  
+}
+arr.push(newProduct);
+fsPromises.writeFile("./data.json",JSON.stringify(arr));
+
+res.json({
+  status:'succes',
+  results:1,
+  data:{
+    newProduct:newProduct, 
+  }
+})
+});
+
+app.put('/api/products/:id',async(req,res)=>{
+//console.log(req);
+const arr=JSON.parse(await fsPromises.readFile("./data.json","utf-8"))
+res.send("work in progress");
+const reqId=parseInt(req.params.id);
+const data=req.body;
+const newArr=arr.map((elem)=>{
+  if(elem.id=reqId)return data;
+  else return elem;
+})
+//console.log(data);
+fsPromises.writeFile("./data.json",JSON.stringify(newArr));
 });
 
 
-
-////port mistake
-//convert json to object 
-//object into json
+app.delete('/api/products/:id',async(req,res)=>{
+  const arr=JSON.parse(await fsPromises.readFile("./data.json","utf-8"))
+  const newArr=arr.filter((elem)=>{
+    if(elem.if==reqId)return false;
+    else return true;
+});
+fsPromises.writeFile("./data.json",JSON.stringify(newArr));
+res.status(204);
+res.json({
+  status:"succes",
+  data:{
+    newProduct:null,
+  }
+})
+});
+app.listen(1400);
